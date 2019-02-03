254d86eab824e3d320bf6bf836141ba498de9b45
swastb <swast10@gmail.com>
2019-02-03 21:24:46 +0530

    adding code

---
+++ mongodb-geo-poc/src/main/java/com/example/mongodbgeopoc/MongodbGeoPocApplication.java
@@ -0,0 +1,14 @@
+package com.example.mongodbgeopoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbGeoPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbGeoPocApplication.class, args);
+	}
+
+}
+
